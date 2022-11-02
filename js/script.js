5ef294b8ca47356975f7dfb7753cf80dd2e24fcb
fathin fadhil <fadhx7@gmail.com>
2022-11-02 21:51:28 +0700

    pwa test #1

--- js/script.js
+++ js/script.js
@@ -9,6 +9,15 @@ var spinner = function () {
 spinner();
 //spinner end
 
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
+
 manualView()
 
 $(document).ready(function() {
